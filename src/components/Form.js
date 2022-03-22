bd81139791d52c208b2daf010107b6c660fe1a09
AKHIL YADAV <akhil.yadav@zenways.io>
2022-03-22 18:33:25 +0530

    use custom hook

--- src/components/Form.js
+++ src/components/Form.js
@@ -66,77 +66,70 @@
 
 // export default Form;
 
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import useInput from "../hooks/UseInput";
 
 function Form(props) {
+  //const [toDo, setName] = useState("");
+  //const [priority, setPriority] = useState("high");
 
-  const [toDo, setName] = useState("");
-  const [priority, setPriority] = useState("high");
+  const [ toDo, bindToDo, resetToDo ] = useInput("");
+  const [ priority, bindPriority, resetPriority ] = useInput("high");
 
-  // handleChange = (event) => {
-  //   this.setState({ toDo: event.target.value });
-  // };
-
-  // handlePriority = (event) => {
-  //   this.setState({ priority: event.target.value });
-  // };
-
-  // handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   this.props.addTask(this.state.toDo, this.state.priority);
-  //   this.setState({ toDo: "", priority: "high" });
-  // };
-
-  function handleChange(e) {
-    setName(e.target.value);
-  }
-  function handlePriority(e) {
-    setPriority(e.target.value);
-  }
+  // function handleChange(e) {
+  //   setName(e.target.value);
+  // }
+  // function handlePriority(e) {
+  //   setPriority(e.target.value);
+  // }
 
   function handleSubmit(e) {
     e.preventDefault();
-    props.addTask(toDo,priority);
-    setName("");
-    setPriority("high");
+    props.addTask(toDo, priority);
+    // setName("");
+    // setPriority("high");
+    resetToDo();
+    resetPriority();
   }
   return (
     <form onSubmit={handleSubmit}>
-    <div className="form-group">
-      <label>Todo*</label>
+      <div className="form-group">
+        <label>Todo*</label>
 
-      <input
-        type="text"
-        className="input"
-        placeholder="add task"
-        value={toDo}
-        onChange={handleChange}
-      />
+        <input
+          type="text"
+          className="input"
+          placeholder="add task"
+          // value={toDo}
+          // onChange={handleChange}
+          {...bindToDo}
+        />
 
-      <label>Priority </label>
+        <label>Priority </label>
 
-      <select
-        className="select"
-        value={priority}
-        onChange={handlePriority}
-      >
-        <option value="high">High</option>
-        <option value="medium">Medium</option>
-        <option value="low">Low</option>
-      </select>
-    </div>
+        <select
+          className="select"
+          // value={priority}
+          // onChange={handlePriority}
+          {...bindPriority}
+        >
+          <option value="high">High</option>
+          <option value="medium">Medium</option>
+          <option value="low">Low</option>
+        </select>
+      </div>
 
-    <div>
-      <button
-        type="submit"
-        className="add-button"
-        disabled={!toDo || !priority}
-      >
-        Add Task
-      </button>
-    </div>
-  </form>
-  )
+      <div>
+        <button
+          type="submit"
+          className="add-button"
+          disabled={!toDo || !priority}
+        >
+          Add Task
+        </button>
+      </div>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
